4228a68b764bf7d1a9b7d12be17f5d7d95ff5da2
Matt Hamil <j.matthew.hamil@gmail.com>
2015-12-29 11:49:46 -0600

    initial commit

---
+++ Easy/1_Easy.js
@@ -0,0 +1,9 @@
+// reddit.com/r/dailyprogrammer/wiki/challenges
+// [2/9/12] Challenge #1 [Easy]
+
+var name, age, username;
+name = prompt("What is your name?");
+age = prompt("What is your age?");
+username = prompt("What is your username?");
+var info = "your name is " + name + ", you are " + age + " years old, and your username is " + username;
+
